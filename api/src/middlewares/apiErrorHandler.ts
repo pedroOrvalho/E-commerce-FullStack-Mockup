77979aa1c9eb37b04844e56cd6714ae665ee755e
andrea <ngocanhhsgs@gmail.com>
2023-07-07 13:26:30 +0300

    add files

---
+++ api/src/middlewares/apiErrorHandler.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+import ApiError from "../helpers/apiError";
+
+export default function (error: ApiError, req: Request, res: Response) {
+  res.status(error.statusCode).json({
+    status: "error",
+    statusCode: error.statusCode,
+    message: error.message,
+  });
+}
