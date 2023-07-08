331bdb27bf703470798fa4da6ac7fae02ec16a68
pedro-orvalho <pedro.orvalho.com>
2023-07-08 02:38:51 +0200

    server.ts added

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import Express from "express";
 
-const app = express();
+const app = Express();
+
 
 export default app;
--- api/src/server.ts
+++ api/src/server.ts
@@ -7,8 +7,16 @@ dotenv.config();
 
 const port = 4000;
 
-mongoose.connect(process.env.MONGODB_URI as string).then(() => {
-  app.listen(port, () => {
-    console.log(`server is running at port ${port}`);
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is running at port ${port}`);
+    });
+  })
+  .catch((error: Error) => {
+    console.log(
+      "MongoDB connection error. Please make sure the database connection is correct."
+    );
+    process.exit(1);
   });
-});
