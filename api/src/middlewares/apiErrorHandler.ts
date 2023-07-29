1d5fa8d71f4ecbb31edbcefde77fbda14cc87a69
pedro-orvalho <pedro.orvalho.com>
2023-07-29 18:02:37 +0200

    deploy issues

--- api/src/middlewares/apiErrorHandler.ts
+++ api/src/middlewares/apiErrorHandler.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import ApiError from "../helpers/apiError";
 
 export default function (error: ApiError, req: Request, res: Response) {
-  res.status(error.statusCode).json({
+  return res.status(error.statusCode).json({
     status: "error",
     statusCode: error.statusCode,
     message: error.message,
